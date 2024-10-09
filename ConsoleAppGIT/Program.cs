a924902fbb24007f061b4c113f3006433c67ea58
HarmeshKr <harmesh7878@gmail.com>
2024-10-09 15:29:28 +0530

    Console App Project Created

---
+++ ConsoleAppGIT/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleAppGIT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
